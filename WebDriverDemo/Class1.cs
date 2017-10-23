ea5f17c8eef24bd10e8b77fc742d48c0f2018e15
Косинский Антон <tekishiromail@gmail.com>
2017-10-23 16:56:29 +0300

    no message

---
+++ WebDriverDemo/Class1.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDriverDemo
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void oneCanOpenGoogle()
+        {
+            IWebDriver chrome = new ChromeDriver();
+            chrome.Navigate().GoToUrl("https://github.com/login");
+
+            IWebElement inputLogin = chrome.FindElement(By.XPath("//input[@id='login_field']"));
+            inputLogin.SendKeys("testautomationuser");
+
+            IWebElement inputPassword = chrome.FindElement(By.XPath("//input[@id='password']"));
+            inputPassword.SendKeys("Time4Death!");
+
+            IWebElement loginButton = chrome.FindElement(By.XPath("//input[@type='submit']"));
+            loginButton.Click();
+        }
+    }
+}
